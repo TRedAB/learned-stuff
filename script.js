fd289abbb65d6793f21183ff9fb69be1c916478c
RedAB <redab.0755@gmail.com>
2025-10-06 22:10:56 -0600

    coso

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const pokeImg = document.getElementById("poke");
 const loadingDef = (document.getElementById("poke").src =
   "https://imgs.search.brave.com/7iVraPqy7eVIf7lcVuIALEHUeO6w3X5llUFdoCt0XlU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50ZW5vci5jb20v/dXB5MENaNkMwSTBB/QUFBbS9lZXZlZS1w/b2tlbW9uLndlYnA");
 const descTag = document.getElementById("descPoke");
+/* const defaultStyle = {
+  fondo : containerPok.style.backgroundColor = "#FF00FF"
+} */
 
 const bgTipos = {
   normal: "#A8A77A",
@@ -47,17 +50,46 @@ async function obtenerPokemon() {
     const descripcion = {
       id: "ID: " + data.id,
       altu : "Altura: " + data.height,
-      tips: `Tipos: ${data.types[0].type.name}${data.types[1] ? ' ' + data.types[1].type.name : ''}`   
+      tips: `Tipos: ${data.types[0].type.name} ${data.types[1] ? ' ' + data.types[1].type.name : ''}`   
     };
 
     descTag.textContent = `${descripcion.id}\n${descripcion.altu}\n${descripcion.tips}`
     descTag.style.whiteSpace = "pre-line";
     const typoPoke = data.types[0].type.name;
-    console.log(typoPoke);
-    containerPok.style.backgroundColor = bgTipos[typoPoke];
+    const typoPoke2 = data.types[1]?.type.name;
+    if (typoPoke2 !== undefined) {
+      containerPok.style.backgroundImage = `linear-gradient(to right, ${bgTipos[typoPoke]}, ${bgTipos[typoPoke2]})`
+    } else {
+      containerPok.style.backgroundImage = `linear-gradient(to right, ${bgTipos[typoPoke]}, #000000ff)`
+    }
+
   } catch (error) {
+    nombrePoke.textContent = "";
+    descTag.textContent = "";
+    containerPok.style.backgroundImage = "none"
+    containerPok.style.backgroundColor = "#f5d498ff";
     pokeText.textContent = "Error al obtener el Pokémon";
-    pokeImg.src =
-      "assets/error.png";
+    pokeImg.src = "assets/error.png";
   }
 }
+
+
+const containerCata = document.getElementById("containerCatalog")
+
+async function catalogPokes() {
+  const url = "https://pokeapi.co/api/v2/pokemon/"
+  const respuesta2 = await fetch(url)
+  const datos = await respuesta2.json()
+  const pokemons = datos.respuesta2
+  pokemons.forEach(element => {
+    const containerCuadro = document.createElement('div')
+    containerCuadro.classList.add('pokeIndividual')
+    const spritePoke = document.createElement('img')
+    spritePoke = datos.sprites.front_default
+    containerCuadro.appendChild(spritePoke)
+    
+  });
+  console.log(datos);
+  
+}
+catalogPokes()
